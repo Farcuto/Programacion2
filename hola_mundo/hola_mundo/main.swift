d07526b62f38feb44cb4d5462cc36e9fee013637
Fernando Rodriguez <fernandor@MacBook-Pro-de-Fernando.local>
2020-11-04 20:53:02 -0400

    Dentro de hola_mundo se encuentra el codigo de swift

---
+++ hola_mundo/hola_mundo/main.swift
@@ -0,0 +1,16 @@
+
+import Foundation // añadir a libreria
+
+print("Fernando Rodriguez 1096552") // imprimir por pantalla
+print("")
+
+print("Bienvenidos a mi primer programa")
+print("")
+
+print("Introduzca su nombre")
+print("")
+print("")
+let nombre = readLine()! //entrada
+
+print("Hola",nombre) // imprimir saludo personalizado
+
